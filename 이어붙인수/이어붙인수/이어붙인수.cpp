3f926b05e0e5d03b34c67966d124a9f0a5394775
yhk <yunhk2001@gmail.com>
2023-11-28 21:14:50 +0900

    문제풀이

---
+++ 이어붙인수/이어붙인수/이어붙인수.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+
+    int even = 0, holes = 0;
+    for (int i = 0; i < num_list.size(); ++i) {
+        if (num_list[i] % 2 == 0) {
+            even *= 10;
+            even += num_list[i];
+        }
+        else {
+            holes *= 10;
+            holes += num_list[i];
+        }
+    }
+
+    answer = even + holes;
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> value = {3, 4, 5, 2, 1};
+    int result = 0;
+    result = solution(value);
+
+    cout << result << "\n";
+    return 0;
+}
---
+++ 조건에맞게수열변환하기3/조건에맞게수열변환하기3/조건에맞게수열변환하기3.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    vector<int> answer;
+
+    if(k % 2 == 0) {
+        for (int i : arr) {
+            answer.push_back(i + k);
+        }
+    }
+    else {
+        for (int i : arr) {
+            answer.push_back(i * k);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> vec = {1, 2, 3, 100, 99, 98};
+    vector<int> result = solution(vec, 3);
+
+    for (int i : result) {
+        cout << i << ' ';
+    }
+
+    return 0;
+}
