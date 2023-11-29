4732876df7ccaca512e8e6b8493fa355914ff3cd
yhk <yunhk2001@gmail.com>
2023-11-29 21:36:56 +0900

    basic

---
+++ 0떼기/0떼기/0떼기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+
+    /*string answer = to_string(stoi(n_str));*/
+    string answer = "";
+
+    bool flag = false;
+    for (int i = 0; i < n_str.size(); ++i) {
+        
+
+        if (n_str[i] != '0')
+            flag = true;
+    }
+    return answer;
+}
+
+int main()
+{
+    
+}
---
+++ 배열의길이에따라다른연산하기/배열의길이에따라다른연산하기/배열의길이에따라다른연산하기.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int n) {
+    vector<int> answer;
+
+    bool flag = arr.size() % 2 == 0 ? true : false;
+    for (int i = 0; i < arr.size(); ++i) {
+        if (flag) {
+            if (i & 1)
+                answer.push_back(arr[i] + n);
+            else
+                answer.push_back(arr[i]);
+        }
+        else {
+            if (i & 1)
+                answer.push_back(arr[i]);
+            else
+                answer.push_back(arr[i]+n);
+        }
+    }
+
+    return answer;
+
+    /*for (int i = (arr.size() + 1) % 2; i < arr.size(); i += 2)
+        arr[i] += n;
+    return arr;*/
+}
+
+int main()
+{
+    vector<int> vec = {49, 12, 100, 276, 33};
+
+    vector<int> result = solution(vec, 27);
+
+    for (int n : result) {
+        cout << n << " ";
+    }
+
+}
